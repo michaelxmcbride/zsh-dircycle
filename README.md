27a37c89d89545dffab031d2a792fd493e52616f
Michael McBride <michaelxmcbride@icloud.com>
2017-06-17 01:25:50 -0400

    Init

---
+++ README.md
@@ -0,0 +1,27 @@
+# zsh-dircycle
+> Cycle through the directory stack
+
+## Installation
+
+### Plugin Manager
+
+You can use the [Zsh plugin manager](https://github.com/search?o=desc&q=zsh+plugin+manager&s=stars&type=Repositories&utf8=%E2%9C%93) of your choice to install `zsh-dircycle`.  For example, if using [Antigen](https://github.com/zsh-users/antigen), add the following line to your `.zshrc` file:
+```sh
+antigen bundle michaelxmcbride/zsh-dircycle
+```
+
+### Manual
+
+Clone this repository:
+```sh
+git clone https://github.com/michaelxmcbride/zsh-dircycle.git
+```
+
+Source `dircycle.zsh` in your `.zshrc` file:
+```sh
+source /path/to/dircycle.zsh
+```
+
+## Usage
+
+Press `Ctrl+Shift+Right` to move forwards and `Ctrl+Shift+Left` to move backwards.
---
+++ dircycle.zsh
@@ -0,0 +1,43 @@
+# Cycle through the directory stack with Ctrl+Shift+Left/Right. Direction follows
+# the order in which directories were visited, like left/right arrows do in a
+# browser.
+
+_dircycle_update_cycled() {
+    setopt localoptions nopushdminus
+
+    [[ ${#dirstack} -eq 0 ]] && return
+
+    while ! builtin pushd -q $1 &>/dev/null; do
+        # A missing directory was found: pop it out of the dir stack
+        builtin popd -q $1
+
+        # Stop trying if there are no more directories in the dir stack
+        [[ ${#dirstack} -eq 0 ]] && break
+    done
+
+    if typeset -f prompt_pure_setup > /dev/null; then
+        # Trigger a prompt update if using Pure (https://github.com/sindresorhus/pure)
+        prompt_pure_async_tasks
+        prompt_pure_preprompt_render
+    else
+        zle reset-prompt
+    fi
+}
+
+_dircycle_insert_cycled_left() {
+    _dircycle_update_cycled +1 || true
+}
+
+zle -N _dircycle_insert_cycled_left
+
+_dircycle_insert_cycled_right() {
+     _dircycle_update_cycled -0 || true
+}
+
+zle -N _dircycle_insert_cycled_right
+
+# Ctrl+Shift+Left (previous)
+bindkey "\e[1;6D" _dircycle_insert_cycled_left
+
+# Ctrl+Shift+Right (next)
+bindkey "\e[1;6C" _dircycle_insert_cycled_right
